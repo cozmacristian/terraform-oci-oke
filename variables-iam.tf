d1b4b82243814a64352f931a36961e1edea97641
Cristian Cozma <cristian.cozma@oracle.com>
2026-02-05 11:53:57 +0200

    Add Karpenter support

--- variables-iam.tf
+++ variables-iam.tf
@@ -184,6 +184,33 @@ variable "create_iam_worker_policy" {
   }
 }
 
+variable "create_iam_karpenter_policy" {
+  default     = "auto"
+  description = "LA: Limited Availability / Whether to create an IAM dynamic group and policy rules for Karpenter management. Ignored when 'create_iam_resources' is false."
+  type        = string
+  validation {
+    condition     = contains(["never", "auto", "always"], var.create_iam_karpenter_policy)
+    error_message = "Accepted values are never, auto, or always"
+  }
+}
+
+variable "karpenter_optional_policies" {
+  default = {
+    capacity_reservation     = false
+    compute_clusters         = false
+    cluster_placement_groups = false
+    defined_tags             = false
+  }
+  description = "LA: Limited Availability / Whether to create the optional IAM policies for Karpenter management. Depends on configuration of associated component when set to 'auto'. Ignored when 'create_iam_resources' is false."
+  type        = map(string)
+}
+
+variable "karpenter_worker_compartments" {
+  default     = []
+  description = "LA: Limited Availability / Compartments where karpenter will create worker nodes. Ignored when 'create_iam_resources' is false."
+  type        = list(string)
+}
+
 # Tagging
 
 variable "create_iam_tag_namespace" {
